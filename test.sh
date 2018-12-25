c860b2bb0598ee27e2d21a991bd7169197d3af40
Kyle E. Mitchell <kyle@kemitchell.com>
2018-12-25 13:08:17 -0800

    Remove old LAMOS test case and scripts

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-for lamos in test/*.lamos; do
-  npx -q lamos-to-json < "$lamos" > "$lamos.json"
-done
-
-for json in text/*.json; do
-  npx -q ajv-cli validate -s schema.json -d "$json"
-done
