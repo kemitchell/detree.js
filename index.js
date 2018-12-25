f76a7155065e15396c57d5bfb2f7a55b3bc49346
Kyle E. Mitchell <kyle@kemitchell.com>
2018-12-25 12:53:16 -0800

    Fix id and goto validation

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-var ajv = require('ajv')
+var AJV = require('ajv')
 var lamos = require('lamos')
 var schema = require('./schema')
+
+var ajv = new AJV()
+var validate = ajv.compile(schema)
 
 exports.parse = function (text) {
   return lamos.parse(text)
 }
 
 exports.validate = function (tree) {
-  var valid = ajv.validate(schema, tree)
-  if (!valid) return tree.errors
-  if (!allGoToTargetsValid(tree)) return false
-  if (!allQuestionsReferenced(tree)) return false
-  return true
+  var valid = validate(tree)
+  if (!valid) return validate.errors
+  var errors = []
+  errors = errors.concat(allGoToTargetsValid(tree))
+  errors = errors.concat(allQuestionsReferenced(tree))
+  return errors
 }
 
 function allGoToTargetsValid (tree) {
+  var errors = []
   var ids = Object.keys(tree)
   for (var questionIndex = 0; questionIndex < tree.length; questionIndex++) {
     var question = tree[questionIndex]
@@ -23,20 +28,33 @@ function allGoToTargetsValid (tree) {
       if (answer.hasOwnProperty('goto')) {
         var goto = answer.goto
         if (ids.indexOf(goto) === -1) {
-          return false // invalid goto
+          errors.push(
+            'Question ' + question.id + ', ' +
+            'answer "' + answer.text + '"' +
+            ' references nonexistent ' + answer.goto +
+            '.'
+          )
         }
       }
     }
   }
-  return true
+  return errors
 }
 
 function allQuestionsReferenced (tree) {
-  return Object.keys(tree).every(function (id) {
-    return tree.some(function (question) {
-      return question.answers.some(function (answer) {
-        return answer.goto === id
+  var errors = []
+  Object.keys(tree).forEach(function (id) {
+    if (id === 'start') return
+    var referenced = Object.values(tree)
+      .some(function (question) {
+        return Object.values(question.answers)
+          .some(function (answer) {
+            return answer.goto === id
+          })
       })
-    })
+    if (!referenced) {
+      errors.push('No answer references ' + id + '.')
+    }
   })
+  return errors
 }
